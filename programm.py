a27852d24d02bbfb91cfe1caf28e4a9f5216d0b6
Yaroslav <test@test.com>
2023-04-11 22:40:07 +0300

    oweride string in programm.py

--- programm.py
+++ programm.py
@@ -2,7 +2,7 @@ import turtle
 
 turtle.setup(500, 500)
 wn = turtle.Screen()
-wn.bgcolor("lightgreen")
+wn.bgcolor("lightgvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvreen")
 # wn.title("Карта прохода по помещению")
 
 tess = turtle.Turtle()
